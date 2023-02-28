11993c1f306bec31b61b490734f93c3228229940
Victor Ribero <victor.ribero3@gmail.com>
2023-02-28 12:52:40 +0100

    refactor: move into domain folders

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './Application/app.service';
 
 @Module({
   imports: [],
