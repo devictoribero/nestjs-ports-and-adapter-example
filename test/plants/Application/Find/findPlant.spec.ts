6885e6a1b52ba1fcc750ceb0d2d390b2aca8b0e6
Victor Ribero <victor.ribero3@gmail.com>
2023-03-01 10:45:42 +0100

    test: create test for finding plant

---
+++ test/plants/Application/Find/findPlant.spec.ts
@@ -0,0 +1,18 @@
+import { FindPlant } from 'src/plants/Application/Find/FindPlant';
+import { Uuid } from 'src/shared/Domain/Uuid';
+
+import { EmptyPlantsRepository } from '../../Infrastructure/Persistance/EmptyPlantsRepository';
+
+describe('FindPlant', () => {
+  it('should create a plant', async () => {
+    // Given a plant we want to create
+    const repository = new EmptyPlantsRepository();
+    const findPlant = new FindPlant(repository);
+    const plantUUid = Uuid.random().toString();
+
+    await findPlant.execute(plantUUid);
+
+    expect(repository.save).toHaveBeenCalledTimes(1);
+    expect(repository.save).toBeCalledWith(plantUUid);
+  });
+});
